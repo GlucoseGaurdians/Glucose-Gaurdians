e6994894681aa0c57eaa4afb95054fe9bd3e62c7
Suzy Le Bel <suzy.lebel@gmail.com>
2021-02-09 21:21:26 -0800

    Navbar styling updated with correct file pathing

--- client/src/components/SharedComponents/Navbar.js
+++ client/src/components/SharedComponents/Navbar.js
@@ -1,34 +1,39 @@
 import React from 'react'
 import { Card, Button, Alert, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap'
-import logo from "../logo";
+import img from "../Images/logo.png";
+
 
 export default function NavbarComponent() {
     return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">
-                    <img
-                        alt=""
-                        src={logo}
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{' '}
-                     Glucose Guardians
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="#home">
+                <img src={img}
+                 alt ="logo"
+                    width="40"
+                    height="40"
+                    className="d-inline-block"
+                />{' '}
+                      Glucose Guardians
                  </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Logout</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="bloodsugar">Track Blood Sugar</Nav.Link>
+                    <Nav.Link href="medication">Manage My Medication</Nav.Link>
+                    <NavDropdown title="More" id="basic-nav-dropdown">
+                        
+                        <NavDropdown.Item href="logout">Logout</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="contact">Contact Us</NavDropdown.Item>
+                        <NavDropdown.Item href="about">About Us</NavDropdown.Item>
+                    </NavDropdown>
+                    <Form class="form-inline my-2 my-lg-0 justify-content-right">
+      <Button class="btn btn-danger my-2 my-sm-0" type="logout">Logout</Button>
+    </Form>
+                
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
