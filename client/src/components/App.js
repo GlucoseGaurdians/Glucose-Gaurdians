0cc9b2266d2f281a8c3e9f9818c8a97333457811
chickey49 <camslens@gmail.com>
2021-02-09 18:26:40 -0800

    oops editing same files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import { AuthProvider } from '../contexts/AuthContext'
 
 import Dashboard from './Dashboard'
 import BloodSugarPage from './BloodSugarPage'
+import BloodSugGraph from './bloodSugSubComps/BloodSugGraph'
 import Medication from './Medication'
 import Questions from './Questions'
 import Signup from './Signup'
@@ -19,6 +20,7 @@ function App() {
                     <Route exact path='/login' component={Login} />
                     <Route exact path='/signup' component={Signup} />
                     <PrivateRoute exact path='/bloodsugar' component={BloodSugarPage} />
+                    <PrivateRoute exact path='/bloodsugar/graph' component={BloodSugGraph} />
                     <PrivateRoute exact path='/medication' component={Medication} />
                     <PrivateRoute exact path='/questions' component={Questions} />
                 </Switch>
--- client/src/components/BloodSugarPage.js
+++ client/src/components/BloodSugarPage.js
@@ -1,17 +1,1 @@
-import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap'
-import Navbar from './SharedComponents/Navbar'
-import LineChart from './SharedComponents/Chart.js'
-import BottomMenuList from './SharedComponents/BottomMenuList'
 
-
-// color coded range at the top : add sugar btn : blood sug chart btn : Take meds btn : Nav?
-export default function BloodSugarPage() {
-    return (
-        <div>
-            <Navbar />
-            <LineChart/>
-            <BottomMenuList/>
-        </div>
-    )
-}
