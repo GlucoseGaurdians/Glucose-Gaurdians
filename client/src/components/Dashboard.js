4caacbbe48a5fbc56b668eb425dd33349ab93daa
Mykel Valadez <valadezmykel@gmail.com>
2021-02-11 18:11:35 -0800

    removed unused logo import on dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,27 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Link, useHistory } from 'react-router-dom'
 import NavbarComponent from './SharedComponents/Navbar'
 import DataRangeCard from './SharedComponents/DataRangeCard'
 import BottomMenuList from './SharedComponents/BottomMenuList'
-import logo from './logo';
+
 
 export default function Dashboard() {
     const history = useHistory()
     const [error, setError] = useState("")
     const { currentUser, logout } = useAuth()
 
-    async function handleLogout() {
-        setError('')
+    useEffect(()=> {
 
-        try {
-            await logout()
-            history.push('/login')
-        } catch {
-            setError('Failed to log out')
-        }
-    }
+    },[])
 
     return (
         <div>
