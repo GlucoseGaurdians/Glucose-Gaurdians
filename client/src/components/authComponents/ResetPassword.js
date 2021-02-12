b1832d1973a264d9404ff7d7aee1af9b814c72aa
Mykel Valadez <valadezmykel@gmail.com>
2021-02-12 12:03:11 -0800

    cleaned up reset password js

--- client/src/components/authComponents/ResetPassword.js
+++ client/src/components/authComponents/ResetPassword.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from '../contexts/AuthContext'
 import { Link } from 'react-router-dom'
 
 export default function ResetPassword() {
@@ -24,8 +24,7 @@ export default function ResetPassword() {
             setMessage('Check your inbox for further instructions')
 
         } catch {
-            console.log("failed to reset password")
-            setError('Failed to sign in')
+            setError('Failed to reset password')
         }
 
         setLoading(false)
