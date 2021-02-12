9ea05a1a1b914760510fe2393d6687d976573873
Ian Wren <ian.wren81@gmail.com>
2021-02-12 15:17:09 -0800

    Fixed mongoose connection to create correct database locally

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
-mongoose.connect(process.env.MONGODB_URI || "local/", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/blood_sugar", 
 { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
 
 app.listen(PORT, () => {
