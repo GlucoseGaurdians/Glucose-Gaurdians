85a3d25cd9054dc72461b2a836f761efefaa1484
Mykel Valadez <valadezmykel@gmail.com>
2021-02-11 18:19:45 -0800

    created a place holder get call for dashboard js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,15 +5,25 @@ import { Link, useHistory } from 'react-router-dom'
 import NavbarComponent from './SharedComponents/Navbar'
 import DataRangeCard from './SharedComponents/DataRangeCard'
 import BottomMenuList from './SharedComponents/BottomMenuList'
+import { UseData } from '../contexts/DataContext'
 
 
 export default function Dashboard() {
     const history = useHistory()
     const [error, setError] = useState("")
-    const { currentUser, logout } = useAuth()
+    const { currentUser } = useAuth()
+    const { bloodSugars, setBloodSugars } = UseData()
 
+    // this doesn't work yet
     useEffect(()=> {
+        const id = currentUser.uid
 
+        fetch("http://localhost:3001/api/bloodsugar/"+id)
+        .then(res => res.json())
+        .then((data) => {
+            setBloodSugars(data)
+        })
+        .catch(err => console.log(err))
     },[])
 
     return (
