5105d9bcc21062a39b69f56eb6c8f01f341b8d54
Mykel Valadez <valadezmykel@gmail.com>
2021-02-16 17:24:54 -0800

    cleaned up app js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import PrivateRoute from '../wrappers/PrivateRoute'
 import { AuthProvider } from '../contexts/AuthContext'
-// import DataContext from '../contexts/DataContext'
 import ScreenSize from '../contexts/ScreenSizeContext'
 
 import Dashboard from './Dashboard'
@@ -23,21 +22,19 @@ function App() {
   return (
       <AuthProvider>
         <ScreenSize>
-        {/* <DataContext> */}
-                <Router>
-                    <Switch>
-                        <Route exact path='/login' component={Login} />
-                        <Route exact path='/signup' component={Signup} />
-                        <Route exact path='/update-profile' component={UpdateProfile} />
-                        <Route exact path='/reset-password' component={ResetPassword} />
-                        <PrivateRoute exact path='/' component={Dashboard} />
-                        <PrivateRoute exact path='/bloodsugar' component={BloodSugarPage} />
-                        <PrivateRoute exact path='/bloodsugar/graph' component={BloodSugGraph} />
-                        <PrivateRoute exact path='/medication' component={Medication} />
-                        <PrivateRoute exact path='/questions' component={Questions} />
-                    </Switch>
-                </Router>
-        {/* </DataContext> */}
+          <Router>
+              <Switch>
+                  <Route exact path='/login' component={Login} />
+                  <Route exact path='/signup' component={Signup} />
+                  <Route exact path='/update-profile' component={UpdateProfile} />
+                  <Route exact path='/reset-password' component={ResetPassword} />
+                  <PrivateRoute exact path='/' component={Dashboard} />
+                  <PrivateRoute exact path='/bloodsugar' component={BloodSugarPage} />
+                  <PrivateRoute exact path='/bloodsugar/graph' component={BloodSugGraph} />
+                  <PrivateRoute exact path='/medication' component={Medication} />
+                  <PrivateRoute exact path='/questions' component={Questions} />
+              </Switch>
+          </Router>
         </ScreenSize>
       </AuthProvider>
   );
