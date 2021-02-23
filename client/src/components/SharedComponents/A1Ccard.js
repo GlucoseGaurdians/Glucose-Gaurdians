8de7d960226e4d7a4174195f4abff9b633453dd6
Ian Wren <ian.wren81@gmail.com>
2021-02-23 14:33:55 -0800

    Deleted old display cards and renamed new display cards

--- client/src/components/SharedComponents/A1Ccard.js
+++ client/src/components/SharedComponents/A1Ccard.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import { Card, Container, Row } from 'react-bootstrap'
+import "./card.css";
 
-export default function A1Ccard(props) {
+// switch(props) {
+//     case props.value < 80:
+//         style= backgroundColor: 'orange'
+//       break;
+//     case props.value > 80 < 120:
+//         style= backgroundColor: 'green'
+//       break;
+//       case props.value >= 120:
+//         style= backgroundColor: 'red'
+//       break;
+//   }
+
+export default function A1cCard(props) {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Body style={{backgroundColor: "green"}}>
-            <div >
-                <Card.Title>{props.title}Projected A1C*</Card.Title>
-                <br />
-                <Card.Subtitle className="mb-2 d-flex justify-content-center">A1C(%)*</Card.Subtitle>
-                {/* <Card.Text>{props.value}ml/dl</Card.Text> */}
-                <Card.Text class="display-3 d-flex justify-content-center">5.3</Card.Text>
-                <Card.Text>
-                    Your A1C is in a good range. Good Job! 
+        <Card className="p-1 my-5" style={{ width: '24 rem' }}>
+            <Card.Body className={props.color}>
+                <Card.Title className="textColor">Projected A1C*</Card.Title>
+                <Card.Subtitle className="mb-2 d-flex justify-content-center textColor">A1C(%)*</Card.Subtitle>
+                <Card.Text className="display-3 d-flex justify-content-center textColor">{props.value}</Card.Text>
+                <Card.Text className="textColor">
+                {props.statement}
                     <br/> 
                     *A1C values based on submitted blood tests and may not reflect true A1C.  Talk to your doctor or
                     medical professional about getting an A1C Test.
                 </Card.Text>
-                <Card.Text> {props.range} </Card.Text>
-                </div>
             </Card.Body>
         </Card>
     )
--- client/src/components/SharedComponents/AvgTestCard.js
+++ client/src/components/SharedComponents/AvgTestCard.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Card, Container, Row } from 'react-bootstrap'
-import "./card.css";
+import { Card } from 'react-bootstrap'
 
 // switch(props) {
 //     case props.value < 80:
@@ -14,16 +13,16 @@ import "./card.css";
 //       break;
 //   }
 
-export default function DataRangeCard(props) {
+export default function AvgTestCard(props) {
     return (
-        <Card className="p-1 my-5" style={{ width: '24 rem' }}>
-            <Card.Body className="cardColorGood">
-                <Card.Title className="textColor">{props.title}Last Test</Card.Title>
+       
+       <Card className="p-1 my-5" style={{ width: '24 rem' }}>
+            <Card.Body className={props.color}>
+                <Card.Title className="textColor">Average Blood Sugar</Card.Title>
                 <Card.Subtitle className="mb-2 d-flex justify-content-center textColor">Bloodsugar(mg/dl)</Card.Subtitle>
                 <Card.Text className="display-3 d-flex justify-content-center textColor">{props.value}</Card.Text>
-                <Card.Text className="textColor">{props.description}
-                Your Bloodsugar is a little high.  Keeping track of more blood sugars could give a better idea of 
-                    where your glucose is for the duration of day.
+                <Card.Text className="textColor">
+                {props.statement}
                 </Card.Text>
             </Card.Body>
         </Card>
