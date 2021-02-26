ce72a16cd866aa715d3a2e483d51e38a859a7a2a
Ian Wren <ian.wren81@gmail.com>
2021-02-25 20:28:25 -0800

    Added logic for cards if no data is in the system>'

--- client/src/components/SharedComponents/ReferenceCard.js
+++ client/src/components/SharedComponents/ReferenceCard.js
@@ -6,17 +6,18 @@ export default function ReferenceCard(props) {
     
     return (
        
-       <Card className="p-1 my-5" style={{ width: '24 rem' }}>
-            <Card.Body>
+       <Card>
+            <Card.Body className="reference">
             
             
-                <Card.Title>Color Code Reference</Card.Title>
-                <div className="rounded-circle justify-content-center cardColorGood"></div>
-                <Card.Text>A good range of blood sugar or A1C</Card.Text>
-                <Card.Text><div className="rounded-circle justify-content-center cardColorWarning"></div>
-                Outside of a good range of blood suagr or projected A1C</Card.Text>
-                <Card.Text><div className="rounded-circle justify-content-center cardColorDanger"></div>
-                A high blood sugar or projected A1C</Card.Text>
+                <Card.Title className="textColor">Color Code Reference</Card.Title>
+                
+                <Card.Text className="textColor"><span className="circle cardColorGood"></span>A good range of blood sugar or A1C, typically between 80mg/dl and 120mg/dl</Card.Text>
+                
+                <Card.Text className="textColor"><span className="circle cardColorWarning"></span>Outside of a good range of blood suagr or projected A1C, between 121mg/dl and 160mg/dl or less than 80mg/dl</Card.Text>
+                
+                <Card.Text className="textColor"><span className="circle cardColorDanger"></span>A high blood sugar or projected A1C, above 161mg/dl</Card.Text>
+                
             </Card.Body>
         </Card>
     )
